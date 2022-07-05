901d53773cd6efe9d5e288a445bb79e99d50364c
Abdulrahman Seliem <oldtimeprogrammer@yahoo.com>
2022-07-05 21:46:33 +0200

    Backend unit tests added

---
+++ WorldCitiesAPI.Tests/CitiesController_Tests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Controllers;
+using WorldCitiesAPI.Data;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Tests
+{
+    public class CitiesController_Tests
+    {
+        ///<summary>
+        /// Test the GetCity() method
+        /// </summary>
+        [Fact]
+        public async Task GetCity()
+        {
+            //Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "WorldCities")
+                .Options;
+            using var context = new ApplicationDbContext(options);
+            context.Add(new City
+            {
+                Id = 1,
+                CountryId = 1,
+                Lat = 1,
+                Lon = 1,
+                Name = "Test City1"
+            });
+            context.SaveChanges();
+            var controller = new CitiesController(context);
+            City? city_existing = null;
+            City? city_notExisting = null;
+
+            //Act
+            city_existing = (await controller.GetCity(1)).Value;
+            city_notExisting = (await controller.GetCity(2)).Value;
+
+            //Assert
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
+        }
+    }
+}
